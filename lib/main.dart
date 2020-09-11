b2bf1c853b1a65c49172c5323379bf80b619a47e
pedro.cabral <pedro.cabral@napista.com.br>
2020-09-11 09:38:55 -0300

    feat: add screenshots and gif

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:movieapp/injector_provider.dart';
 import 'package:movieapp/presentation/pages/home/home_page.dart';
 
-//TODO Refactor code UI
-//TODO Readme
-//TODO Open PR
-//TODO Verificar iOS
-//TODO replace all in data
-
 void main() {
   setupInjection();
   runApp(MyApp());
