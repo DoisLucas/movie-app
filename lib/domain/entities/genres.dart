b55634da52ee0d8f3a439e5f8bceac77a2cda400
pedro.cabral <pedro.cabral@napista.com.br>
2020-09-10 23:36:47 -0300

    feat: movie details entities

---
+++ lib/domain/entities/genres.dart
@@ -0,0 +1,9 @@
+class Genres {
+  final int id;
+  final String name;
+
+  Genres({
+    this.id,
+    this.name,
+  });
+}
---
+++ lib/domain/entities/movie_details.dart
@@ -0,0 +1,63 @@
+import 'package:movieapp/domain/entities/production_companies.dart';
+import 'package:movieapp/domain/entities/production_countries.dart';
+import 'package:movieapp/domain/entities/spoken_languages.dart';
+
+import 'belongs_to_collection.dart';
+import 'genres.dart';
+
+class MovieDetail {
+  final bool adult;
+  final String backdropPath;
+  final BelongsToCollection belongsToCollection;
+  final int budget;
+  final List<Genres> genres;
+  final String homepage;
+  final int id;
+  final String imdbId;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final List<ProductionCompanies> productionCompanies;
+  final List<ProductionCountries> productionCountries;
+  final String releaseDate;
+  final int revenue;
+  final int runtime;
+  final List<SpokenLanguages> spokenLanguages;
+  final String status;
+  final String tagline;
+  final String title;
+  final bool video;
+  final double voteAverage;
+
+  MovieDetail({
+    this.adult,
+    this.backdropPath,
+    this.belongsToCollection,
+    this.budget,
+    this.genres,
+    this.homepage,
+    this.id,
+    this.imdbId,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.productionCompanies,
+    this.productionCountries,
+    this.releaseDate,
+    this.revenue,
+    this.runtime,
+    this.spokenLanguages,
+    this.status,
+    this.tagline,
+    this.title,
+    this.video,
+    this.voteAverage,
+  });
+}
+
+
+
